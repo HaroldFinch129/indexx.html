2d9a39fed3ade85401bf4a44278c08954ef2702c
Harold <haroldfinch129@gmail.com>
2021-06-16 22:52:52 +0300

    new

---
+++ app.js
@@ -0,0 +1,56 @@
+
+
+
+
+// console.time();
+// console.log('Hello Clarusway');
+// console.info('some infor');
+// console.warn('some warning');
+// console.error('error!!!');
+
+// console.log('______');
+// var myName = 'Harold';
+// console.log(myName);
+
+// var num = 4;
+// console.log(num);
+// console.log(num*num);
+
+
+// const PI =3.141521;
+
+// let myNum = 5 ;
+// console.log(PI);
+
+// // PI = myNum;
+// // console.log(PI);
+
+// myNum = PI * 4 ;
+// console.log(myNum);
+
+// const SECONDS_IN_A_DAY = 24 * 60 * 60 ;
+// console.log(SECONDS_IN_A_DAY);
+
+// alert('merhaba');
+
+// var country = prompt('Enter Your Country:');
+// console.log('Your country is', country);
+
+
+// var num1 = 42;
+// // if(num1 == 42) {
+// //     // var num2 = 22
+// console.log(num1,num2); 
+
+function func1() {
+    var myName = 'Harold';
+    console.log('from func1 ... ', myName );
+
+}
+
+func1();
+
+
+
+
+
